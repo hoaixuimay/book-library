99abd5503f969fc76e5a77bc0449f3131e72eec4
Hoai Pham <hoaixuimay@gmail.com>
2020-10-25 15:12:07 +0700

    initial

---
+++ src/test/java/com/hoaixuimay/quarkus/NativeBookLibraryResourceIT.java
@@ -0,0 +1,9 @@
+package com.hoaixuimay.quarkus;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeBookLibraryResourceIT extends BookLibraryResourceTest {
+
+    // Execute the same tests but in native mode.
+}
